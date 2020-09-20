6b960a178cb66735b8bddbf82376fda73acf8041
Valentin U <valentin@kangaroorewards.com>
2020-09-20 08:02:03 -0400

    Add phinx

---
+++ app/db/migrations/20200919191058_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateUsersTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $table = $this->table('users', ['collation' => 'utf8mb4_unicode_ci', 'signed' => false]);
+        $table
+            ->addColumn('email', 'string', ['limit' => 50, 'null' => false])
+            ->addColumn('password', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('first_name', 'string', ['limit' => 50, 'null' => true])
+            ->addColumn('last_name', 'string', ['limit' => 50, 'null' => true])
+            ->addColumn('created_at', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'datetime', [
+                'null' => false,
+                'default' => 'CURRENT_TIMESTAMP',
+                'update' => 'CURRENT_TIMESTAMP'
+            ])
+            ->addIndex('email', ['unique' => true])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('users');
+    }
+}
